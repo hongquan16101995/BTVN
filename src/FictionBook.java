403c51ba8146b45b3aaf4469500cd721d8b5f78d
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-07-23 11:47:23 +0700

    addClass

---
+++ src/FictionBook.java
@@ -0,0 +1,29 @@
+public class FictionBook extends Book {
+    private String category;
+
+    public FictionBook() {
+
+    }
+
+    public FictionBook(int bookCode, String name, int price, String author) {
+        super(bookCode, name, price, author);
+    }
+
+    public FictionBook(int bookCode, String name, int price, String author, String category) {
+        super(bookCode, name, price, author);
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "FictionBook{" + super.toString() + "category='" + category + '\'' + '}';
+    }
+}
