9843325e0bc483ddf21a0391643bd6761255d63f
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-07-23 23:19:01 +0700

    addMenuinRunLast

--- src/RunLastByUser.java
+++ src/RunLastByUser.java
@@ -4,27 +4,61 @@ public class RunLastByUser {
     public static void main(String[] args) {
         RunBookManager runBookManager = new RunBookManager();
 
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        switch (number) {
-            case 1:
-                runBookManager.setProgrammingBookList();
-            case 2:
-                runBookManager.setFictionBookList();
-            case 3:
-                runBookManager.getTotalPrice();
-            case 4:
-                runBookManager.checkLanguage();
-            case 5:
-                runBookManager.searchByLinear();
-            case 6:
-                runBookManager.searchByBinary();
-            case 7:
-                runBookManager.sortByChoice();
-            case 8:
-                runBookManager.sortByInsert();
-            case 9:
-                runBookManager.sortByBubble();
-        }
+        boolean cont = true;
+        do {
+            System.out.println("1. Add in list ProgrammingBookList");
+            System.out.println("2. Add in list FictionBookList");
+            System.out.println("3. Calculate total price");
+            System.out.println("4. Check language of ProgrammingBookList");
+            System.out.println("5. Search price by name by linear");
+            System.out.println("6. Search price by name by binary");
+            System.out.println("7. Book list sort by Choice");
+            System.out.println("8. Book list sort by Insert");
+            System.out.println("9. Book list sort by Bubble");
+            System.out.println("0. Exit");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your choice: ");
+            int number = sc.nextInt();
+            switch (number) {
+                case 1:
+                    runBookManager.setProgrammingBookList();
+                    break;
+                case 2:
+                    runBookManager.setFictionBookList();
+                    break;
+                case 3:
+                    cont = false;
+                    runBookManager.getTotalPrice();
+                    break;
+                case 4:
+                    cont = false;
+                    runBookManager.checkLanguage();
+                    break;
+                case 5:
+                    cont = false;
+                    runBookManager.searchByLinear();
+                    break;
+                case 6:
+                    cont = false;
+                    runBookManager.searchByBinary();
+                    break;
+                case 7:
+                    cont = false;
+                    runBookManager.sortByChoice();
+                    break;
+                case 8:
+                    cont = false;
+                    runBookManager.sortByInsert();
+                    break;
+                case 9:
+                    cont = false;
+                    runBookManager.sortByBubble();
+                    break;
+                case 0:
+                    cont = false;
+                    System.out.println("Goodbye!");
+                    break;
+            }
+        }while (cont);
     }
 }
